4f1ecba05d69e2cd22f1f75f20929e6a270cf0fe
Piet <senlinonline@gmail.com>
2011-04-13 09:45:00 -0700

    Edited functions.php via GitHub

--- functions.php
+++ functions.php
@@ -30,9 +30,9 @@
     
     if (function_exists('register_sidebar')) {
     	register_sidebar(array(
-    		'name' => 'Sidebar Widgets',
+    		'name' => __( 'Sidebar Widgets', 'h5rl' ),
     		'id'   => 'sidebar-widgets',
-    		'description'   => 'These are widgets for the sidebar.',
+    		'description'   => __( 'These are widgets for the sidebar.', 'h5rl' ),
     		'before_widget' => '<div id="%1$s" class="widget %2$s">',
     		'after_widget'  => '</div>',
     		'before_title'  => '<h2>',
