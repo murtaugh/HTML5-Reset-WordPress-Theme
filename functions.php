799fc5667a2625a9d59b57cee49073c9fd7967b1
Jeff Sebring <jeff@jeffsebring.com>
2011-04-18 22:10:48 -0700

    Added WordPress 3.1 Post Formats, with body class mod.

--- functions.php
+++ functions.php
@@ -1,15 +1,15 @@
 <?php
-	
+
 	// Add RSS links to <head> section
 	automatic_feed_links();
-	
+
 	// Load jQuery
 	if ( !is_admin() ) {
 	   wp_deregister_script('jquery');
 	   wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"), false);
 	   wp_enqueue_script('jquery');
 	}
-	
+
 	// Clean up the <head>
 	function removeHeadLinks() {
     	remove_action('wp_head', 'rsd_link');
@@ -17,7 +17,7 @@
     }
     add_action('init', 'removeHeadLinks');
     remove_action('wp_head', 'wp_generator');
-    
+
     if (function_exists('register_sidebar')) {
     	register_sidebar(array(
     		'name' => 'Sidebar Widgets',
@@ -30,4 +30,19 @@
     	));
     }
 
+/*
+Default Post Formats insert the class in the post div, this adds them to the body class for more design control.
+Jeff Sebring - http://jeffsebring.com/enabling-killer-wordpress-post-formats/
+*/
+function killer_post_formats()	{
+    add_theme_support( 'post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'audio', 'chat', 'video')); // Add 3.1 post format theme support.
+    function custom_body_class($classes) {
+    $format = get_post_format( $post_id ); // Determine current post format
+    $classes[] .= $format; // Add current format to body class
+    return $classes;
+    }
+    add_filter('body_class', 'custom_body_class');
+}
+killer_post_formats();
 ?>
+
