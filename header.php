6569e348d8785b73e25d87711cb83704abb0eb2f
Tim Murtaugh <tim@monkeydo.biz>
2011-06-20 13:07:26 -0400

    removed bad character on line 78

--- header.php
+++ header.php
@@ -75,7 +75,7 @@
 	device-width : Occupy full width of the screen in its current orientation
 	initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
 	maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width -->
-	<!-- Uncomment to use Ñ use thoughtfully!
+	<!-- Uncomment to use; use thoughtfully!
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
 	-->
 	
