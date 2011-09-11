07aed7030eb7b1fc2ffd2f5e9397a307906c97c2
Paul Burgess <delargerock@gmail.com>
2011-09-12 00:23:12 +0200

    Edited line 29, 33, 49 & 53. The title and meta title did not cater for a custom homepage. To include this, is_front_page has been added. In addition, had to add 'is not front page' (line 29 & 49) as it adds a - to the front of the page title if using a custom homepage.

--- header.php
+++ header.php
@@ -26,11 +26,11 @@
 		         wp_title(''); echo ' Archive - '; }
 		      elseif (is_search()) {
 		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
-		      elseif (!(is_404()) && (is_single()) || (is_page())) {
+		      elseif (!(is_404()) && (is_single()) || (is_page() && !is_front_page())) {
 		         wp_title(''); echo ' - '; }
 		      elseif (is_404()) {
 		         echo 'Not Found - '; }
-		      if (is_home()) {
+		      if (is_home() || is_front_page()) {
 		         bloginfo('name'); echo ' - '; bloginfo('description'); }
 		      else {
 		          bloginfo('name'); }
@@ -46,11 +46,11 @@
 		         wp_title(''); echo ' Archive - '; }
 		      elseif (is_search()) {
 		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
-		      elseif (!(is_404()) && (is_single()) || (is_page())) {
+		      elseif (!(is_404()) && (is_single()) || (is_page() && !is_front_page())) {
 		         wp_title(''); echo ' - '; }
 		      elseif (is_404()) {
 		         echo 'Not Found - '; }
-		      if (is_home()) {
+		      if (is_home() || is_front_page()) {
 		         bloginfo('name'); echo ' - '; bloginfo('description'); }
 		      else {
 		          bloginfo('name'); }
