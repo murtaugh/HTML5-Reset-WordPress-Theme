9246a6afd2e0dc33caf5d8242ec369c346d4bba7
Alberto Ramacciotti <ramacciotti.alberto@gmail.com>
2012-03-03 02:21:16 +0100

    Removed spaces, converted tabs to 4chars, fixed redundant php closing tags

--- _/js/functions.js
+++ _/js/functions.js
@@ -1,23 +1,19 @@
 // remap jQuery to $
 (function($){})(window.jQuery);
 
-
 /* trigger when page is ready */
 $(document).ready(function (){
-
-	// your functions go here
-
+    // your functions go here
 });
-
 
 /* optional triggers
 
 $(window).load(function() {
-	
+
 });
 
 $(window).resize(function() {
-	
+
 });
 
 */
