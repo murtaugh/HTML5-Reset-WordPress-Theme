2766e37278c589b33233b4af0b3a873b12543ce2
Scott Lyttle <scott.lyttle@gmail.com>
2012-03-20 20:48:49 +1100

    Updated Modernizr, Custom Comments, Direct JQuery Link, Separate Respond File
    Removed Modernizr 1.7 and replaced it with version 2.5.3. I added
    custom comment layout to the functions.php. Updated the direct link to
    JQuery on GCode pointed at the latest 1.7 release. Lastly I moved the
    respond form to the inc folder to keep the loop clean

--- header.php
+++ header.php
@@ -95,7 +95,7 @@
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 	
 	<!-- all our JS is at the bottom of the page, except for Modernizr. -->
-	<script src="<?php bloginfo('template_directory'); ?>/_/js/modernizr-1.7.min.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/_/js/modernizr-2.5.3.min.js"></script>
 	
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
