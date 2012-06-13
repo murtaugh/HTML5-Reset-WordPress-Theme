3f6a47e4ea4f5ac96265805f03ccb9070cffbd3d
Aiham Hammami <aiham@aiham.net>
2012-06-14 01:31:52 +1000

    Fixed the datetime of a post being generated from the date() function to the the_time() function

--- _/inc/meta.php
+++ _/inc/meta.php
@@ -1,5 +1,5 @@
 <footer class="meta">
-	<i>Posted on:</i> <time datetime="<?php echo date(DATE_W3C); ?>" pubdate class="updated"><?php the_time('F jS, Y') ?></time>
+	<i>Posted on:</i> <time datetime="<?php echo the_time(DATE_W3C); ?>" pubdate class="updated"><?php the_time('F jS, Y') ?></time>
 	<span class="byline author vcard">
 		<i>by</i> <span class="fn"><?php the_author() ?></span>
 	</span>
