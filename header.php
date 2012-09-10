05111747859753641a6a8baafbca99a4953566c0
Tim Novinger <tim.novinger@gmail.com>
2012-09-10 09:04:33 -0400

    Cleaned up header and added normal CJ meta code

--- header.php
+++ header.php
@@ -59,16 +59,11 @@
        ?>">
   <meta name="description" content="<?php bloginfo('description') ?>">
 
-  <meta name="google-site-verification" content="">
+  <meta name="google-site-verification" content="<?php echo getcustomfield('google-site-verification') ?>">
   <!-- Speaking of Google, don't forget to set your site up: http://google.com/webmasters -->
 
-  <meta name="author" content="Your Name Here">
-  <meta name="Copyright" content="Copyright Your Name Here 2011. All Rights Reserved.">
-
-  <!-- Dublin Core Metadata : http://dublincore.org/ -->
-  <meta name="DC.title" content="Project Name">
-  <meta name="DC.subject" content="What you're about.">
-  <meta name="DC.creator" content="Who made this site.">
+  <meta name="author" content="<?php bloginfo('name') ?>">
+  <meta name="Copyright" content="Copyright <?php bloginfo('name') ?> <?php echo date('Y') ?>. All Rights Reserved.">
 
   <!--  Mobile Viewport meta tag
   j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag
@@ -79,13 +74,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
   -->
 
-  <link rel="shortcut icon" href="<?php bloginfo('template_directory') ?>/_/img/favicon.ico">
+  <link rel="icon" href="<?php bloginfo('template_directory') ?>/_/images/favicon.gif">
+  <link rel="shortcut icon" href="<?php bloginfo('template_directory') ?>/_/images/favicon.gif">
   <!-- This is the traditional favicon.
      - size: 16x16 or 32x32
      - transparency is OK
      - see wikipedia for info on browser support: http://mky.be/favicon/ -->
 
-  <link rel="apple-touch-icon" href="<?php bloginfo('template_directory') ?>/_/img/apple-touch-icon.png">
+  <link rel="apple-touch-icon" href="<?php bloginfo('template_directory') ?>/_/images/apple-touch-icon.png">
   <!-- The is the icon for iOS's Web Clip.
      - size: 57x57 for older iPhones, 72x72 for iPads, 114x114 for iPhone4's retina display (IMHO, just go ahead and use the biggest one)
      - To prevent iOS from applying its styles to the icon name it thusly: apple-touch-icon-precomposed.png
@@ -102,9 +98,8 @@
   <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ) ?>
 
   <?php wp_head() ?>
-
 </head>
-
+<?php flush() ?>
 <body <?php body_class() ?>>
 
   <div id="page-wrap"><!-- not needed? up to you: http://camendesign.com/code/developpeurs_sans_frontieres -->
