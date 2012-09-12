9e12cd5302cecb9299019b5aa21a8adbc1c6c555
Tim Novinger <tim.novinger@gmail.com>
2012-09-12 12:05:18 -0400

    Chore: Setup Google Analytics snippet

--- footer.php
+++ footer.php
@@ -10,12 +10,14 @@
 <script src="<?php echo get_template_directory_uri() ?>/_/js/functions.js"></script>
 
 <!-- Asynchronous google analytics; this is the official snippet.
-   Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.
-
+   Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.  -->
+<?php
+  @$analytics_id = getcustomfield('google_analytics_id') ?: '';
+  if (!empty($analytics_id)) :
+?>
 <script>
-
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-XXXXXX-XX']);
+  _gaq.push(['_setAccount', '<?php echo $analytics_id ?>']);
   _gaq.push(['_trackPageview']);
 
   (function() {
@@ -23,9 +25,8 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-
 </script>
--->
+<?php endif ?>
 
 </body>
 </html>
