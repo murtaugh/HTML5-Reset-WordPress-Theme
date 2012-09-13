9edf4f36b23bb51c6e58a735d64b1ec9d46ccac6
Tim Novinger <tim.novinger@gmail.com>
2012-09-13 14:40:55 -0400

    Chore: Added placeholder JS to functions.js

--- _/js/functions.js
+++ _/js/functions.js
@@ -5,7 +5,28 @@
 /* trigger when page is ready */
 $(document).ready(function (){
 
-	// your functions go here
+  // ------------------------------
+  // Form Input Placeholder
+  // ------------------------------
+  if (!Modernizr.inputtypes.email)
+  {
+    $('input[placeholder], textarea[placeholder]').each(function(i, input){
+      var $input = $(input);
+
+      // Initially load the placeholder value
+      if ($input.val() == '') { $input.val($input.attr('placeholder')); }
+
+      $input
+        .bind('focusin', function(){
+          var $this = $(this);
+          if ($this.val() == $input.attr('placeholder')) { $this.val(''); }
+        })
+        .bind('focusout', function(){
+          var $this = $(this);
+          if ($this.val() == '') { $this.val($input.attr('placeholder')); }
+        });
+    });
+  }
 
 });
 
@@ -13,11 +34,11 @@ $(document).ready(function (){
 /* optional triggers
 
 $(window).load(function() {
-	
+
 });
 
 $(window).resize(function() {
-	
+
 });
 
 */
