273bbdaef2207181e12339fbd792bd68c0f77450
Tim Novinger <tim.novinger@gmail.com>
2012-10-10 18:52:28 -0400

    Bugfix: corrected bad characters

--- _/inc/colorjar/functions.php
+++ _/inc/colorjar/functions.php
@@ -172,7 +172,7 @@
 
 
   // Trim strings by words
-  function neat_trim($str, $n, $delim='…') {
+  function neat_trim($str, $n, $delim='...' {
     $len = strlen($str);
     if ($len > $n) {
       preg_match('/(.{' . $n . '}.*?)\b/', $str, $matches);
