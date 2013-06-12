9ac5512adfb329247564709b3256dc09c2942506
Edouard Duplessis <e.duplessis@businessfamilies.org>
2013-06-12 14:00:40 -0400

    You should never put directly any script or style in the header

--- header.php
+++ header.php
@@ -63,14 +63,6 @@
 		 - Transparency is not recommended (iOS will put a black BG behind the icon) -->';
 	} ?>
 
-	<!-- concatenate and minify for production -->
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/reset.css" />
-	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" />
-	
-	<!-- This is an un-minified, complete version of Modernizr. 
-		 Before you move to production, you should generate a custom build that only has the detects you need. -->
-	<script src="<?php echo get_template_directory_uri(); ?>/_/js/modernizr-2.6.2.dev.js"></script>
-
 	<!-- Application-specific meta tags -->
 	<?php if (true == of_get_option('meta_app_win_name')) {
 	echo '<!-- Windows 8 -->';
