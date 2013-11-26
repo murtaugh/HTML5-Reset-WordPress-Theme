b661fb2d7ce027e88e24f25eddeab0c325d75dd6
Mike Heitzke <heitzke@mikes-mbp.int.asurion.com>
2013-11-26 16:02:20 -0600

    updating landmark roles for header and footer

--- header.php
+++ header.php
@@ -110,7 +110,7 @@
 	<div id="wrapper">
 	<!-- not needed? up to you: http://camendesign.com/code/developpeurs_sans_frontieres -->
 
-		<header id="header" role="header">
+		<header id="header" role="banner">
 			<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<div class="description"><?php bloginfo( 'description' ); ?></div>
 		</header>
