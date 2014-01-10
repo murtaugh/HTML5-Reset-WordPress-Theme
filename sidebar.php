0695671a4dbfc40aabc0d132d031333e150ce498
CK Hicks <contact@ckhicks.com>
2014-01-10 16:14:26 -0600

    Long-awaited fixes to sidebar container and widget logic

--- sidebar.php
+++ sidebar.php
@@ -5,9 +5,9 @@
  * @since HTML5 Reset 2.0
  */
 ?>
- <div id="sidebar">
+ <aside id="sidebar">
 
-    <?php if (!function_exists('dynamic_sidebar') && !dynamic_sidebar('Sidebar Widgets')) : else : ?>
+    <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('Sidebar Widgets')) : else : ?>
     
         <!-- All this stuff in here only shows up if you DON'T have any widgets active in this zone -->
 
@@ -34,4 +34,4 @@
 	
 	<?php endif; ?>
 
-</div>
+</aside>
