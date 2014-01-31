41fd3cd4b1b29ba049f6a03ca3e024329c8db4c0
Michal Černý <michal@cerny.me>
2014-01-31 13:23:00 +0100

    Update header.php
    Not loading prefixfree missing temp uri. 
    Modernizr bad version in header

--- header.php
+++ header.php
@@ -83,11 +83,11 @@
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" />
 	
 	<!-- Lea Verou's Prefix Free, lets you use only un-prefixed properties in yuor CSS files -->
-    <script src="_/js/prefixfree.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/_/js/prefixfree.min.js"></script>
 
 	<!-- This is an un-minified, complete version of Modernizr. 
 		 Before you move to production, you should generate a custom build that only has the detects you need. -->
-	<script src="<?php echo get_template_directory_uri(); ?>/_/js/modernizr-2.6.2.dev.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/_/js/modernizr-2.7.0.dev.js"></script>
 
 	<!-- Application-specific meta tags -->
 	<?php
